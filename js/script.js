ba951ecd6e4263e36937f53ffca6ec7e7e6e2d40
PedroSilva4720 <39890429+PedroSilva4720@users.noreply.github.com>
2020-03-21 20:33:11 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+function change() {
+    document.getElementById("boxId").style.height = document.getElementById('height_').value + 'px';
+    document.getElementById("boxId").style.width = document.getElementById('width_').value + 'px';
+}
